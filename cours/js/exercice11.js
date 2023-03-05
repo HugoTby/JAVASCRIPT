e00234a4a87cb3d446e7739c5356126958657ad6
Hugo Tabary <97984737+HugoTby@users.noreply.github.com>
2023-03-05 17:36:47 +0100

    Update exercice11.js

--- cours/js/exercice11.js
+++ cours/js/exercice11.js
@@ -1,7 +1,7 @@
-// const fetchResponsePromise = fetch("http://127.0.0.1/JAVASCRIPT/cours/json/exercice11.json",{method: 'GET'});
-// const PromiseJson = fetchResponsePromise.then(response => response.json())
-// const ResponsePromise = PromiseJson.then(response2 => console.log(JSON.stringify(response2)))
-// PromiseJson.catch(error => VideoColorSpace.log("Le serveur à renvoyé l'erreur : "+ error))
+const fetchResponsePromise = fetch("http://127.0.0.1/JAVASCRIPT/cours/json/exercice11.json",{method: 'GET'});
+const PromiseJson = fetchResponsePromise.then(response => response.json())
+const ResponsePromise = PromiseJson.then(response2 => console.log(JSON.stringify(response2)))
+PromiseJson.catch(error => VideoColorSpace.log("Le serveur à renvoyé l'erreur : "+ error))
 
 // ou sous une forme factorisée :
 
