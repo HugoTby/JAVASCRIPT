c0978c89bf26ab1bb212c295ef01e8accbc00758
Hugo Tabary <97984737+HugoTby@users.noreply.github.com>
2023-03-05 23:15:38 +0100

    JS.05

--- cours/5_exercice11.php
+++ cours/5_exercice11.php
@@ -4,12 +4,18 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" media="screen" href="css/exercice11.css">
     <title>Document</title>
 </head>
 <body>
     <h1 style="text-align: center;color:orange">Exercice 11 sur le JavaScript</h1>
 
-
+    <div class="divA">
+        <div id="MethodeJSON">
+            Cliquez ici pour récuperer le JSON
+        </div>
+    </div>
+    
 
     <script type="text/javascript" src="js/exercice11.js"></script>
 
