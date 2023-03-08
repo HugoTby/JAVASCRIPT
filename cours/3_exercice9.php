0f5e3420a135417abc3a0ed8d5baf9fb8499e373
Hugo Tabary <97984737+HugoTby@users.noreply.github.com>
2023-03-08 16:34:25 +0100

    JS.03

--- cours/3_exercice9.php
+++ cours/3_exercice9.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+<style>.bouton{display:inline-block;padding:10px 20px;font-size:16px;font-weight:700;text-align:center;text-decoration:none;color:#fff;background-color:#4caf50;border-radius:5px;border:none;cursor:pointer}</style>
 <body>
     <h1 style="text-align: center;color:orange">Exercice 9 sur le JavaScript</h1>
 
@@ -13,12 +14,13 @@
     <div class="LesDIVClass" name="LesDIV">coucou</div>
     <div class="LesDIVClass" name="LesDIV">coucou</div>
 
+    <div class="bouton" onclick="modification();" >GetElementByName</div><br><br>
+    <div class="bouton" onclick="modification2();">GetElementByTagName</div><br><br>
+    <div class="bouton" onclick="modification3();">GetElementByClassName</div>
 
 
     <script type="text/javascript" src="js/exercice9.js"></script>
-    <!--<script>modification();</script> -->
-    <!--<script>modification2();</script> -->
-    <script>modification3();</script>
+    
 </body>
 </html>
  
