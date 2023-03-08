810ae08da9da81e1f955129494aceaa5326d83a4
Hugo Tabary <97984737+HugoTby@users.noreply.github.com>
2023-03-08 16:24:30 +0100

    Update exercice9.js

--- cours/js/exercice9.js
+++ cours/js/exercice9.js
@@ -1,12 +1,32 @@
+// 9.1
 function modification() {
     alert("Je vais modifier la div !");
     var modif = document.getElementsByName("LesDIV");
 
-    /*for (var i = 0; i < modif.length; i++) {
-        modif[i].innerHTML = "Hello";
-    }*/
     modif.forEach(function(element) {
         element.innerHTML = "Hello";
     });
-    return "Hello";
+}
+
+
+// 9.2
+
+function modification2() {
+    alert("Je vais modifier la div !");
+    var modif = document.getElementsByTagName("div");
+
+    for (var i = 0; i < modif.length; i++) {
+        modif[i].innerHTML = "Hello";
+    }
+}
+
+// 9.3
+
+function modification3() {
+    alert("Je vais modifier la div !");
+    var modif = document.getElementsByClassName("LesDIVClass");
+    
+    for (var i = 0; i < modif.length; i++) {
+        modif[i].innerHTML = "Hello";
+    }
 }
