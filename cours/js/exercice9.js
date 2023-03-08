4a498a2a6d88e2f41eaecb23461f9a961aedebdd
Hugo Tabary <97984737+HugoTby@users.noreply.github.com>
2023-03-08 15:28:56 +0100

    Update exercice9.js

--- cours/js/exercice9.js
+++ cours/js/exercice9.js
@@ -2,8 +2,11 @@ function modification() {
     alert("Je vais modifier la div !");
     var modif = document.getElementsByName("LesDIV");
 
-    for (var i = 0; i < modif.length; i++) {
+    /*for (var i = 0; i < modif.length; i++) {
         modif[i].innerHTML = "Hello";
-    }
+    }*/
+    modif.forEach(function(element) {
+        element.innerHTML = "Hello";
+    });
     return "Hello";
 }
