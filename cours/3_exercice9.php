9411c5863bbbf7664cc07268e4b2c17199c41e7a
Hugo Tabary <97984737+HugoTby@users.noreply.github.com>
2023-03-08 16:24:54 +0100

    JS.03

--- cours/3_exercice9.php
+++ cours/3_exercice9.php
@@ -9,15 +9,16 @@
 <body>
     <h1 style="text-align: center;color:orange">Exercice 9 sur le JavaScript</h1>
 
-    <div name="LesDIV">coucou</div>
-    <div name="LesDIV">coucou</div>
-    <div name="LesDIV">coucou</div>
-
+    <div class="LesDIVClass" name="LesDIV">coucou</div>
+    <div class="LesDIVClass" name="LesDIV">coucou</div>
+    <div class="LesDIVClass" name="LesDIV">coucou</div>
 
 
 
     <script type="text/javascript" src="js/exercice9.js"></script>
-    <script>modification();</script>
+    <!--<script>modification();</script> -->
+    <!--<script>modification2();</script> -->
+    <script>modification3();</script>
 </body>
 </html>
  
