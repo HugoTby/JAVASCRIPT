422378b9befc67f4a80a90a870ac4f07acd59cb3
Hugo Tabary <97984737+HugoTby@users.noreply.github.com>
2023-03-08 15:29:38 +0100

    JS.03

--- cours/3_exercice9.php
+++ cours/3_exercice9.php
@@ -4,7 +4,6 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <script type="text/javascript" src="js/exercice9.js"></script>
     <title>Document</title>
 </head>
 <body>
@@ -17,6 +16,7 @@
 
 
 
+    <script type="text/javascript" src="js/exercice9.js"></script>
     <script>modification();</script>
 </body>
 </html>
