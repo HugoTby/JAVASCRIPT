5041324dd20e49f47a1e9dca9c0b1355b96e1c25
Hugo Tabary <97984737+HugoTby@users.noreply.github.com>
2023-03-10 14:57:34 +0100

    Add files via upload

---
+++ cours/TD_JS.01/js/exercice1.js
@@ -0,0 +1,6 @@
+/* 
+En cliquant sur un bouton demander à utilisateur de saisir une longueur et une largeur
+(prompt) et afficher le résultat du périmètre puis de l’air d’un rectangle (alert())
+Améliorer le code pour que ce soit une fonction qui soit appelé au clic du bouton
+*/
+
